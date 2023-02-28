78ffed13b330daaf4f6360e7db6e12634b2b7ed8
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2023-02-28 22:55:50 +0100

    works, but throws lots of "unresolved reference" errors in build.gradle.kts

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,51 @@
+import kotlin.random.Random
+
+fun main() {
+    val numberString = generateNumberString()
+    println("Please enter your guess (4 digit number):")
+    gameLoop@ while (true){
+        val guess = readLine()
+        if (guess == null || guess.length != 4) {
+            println("please enter a 4 digit number")
+            continue;
+        }
+        if (guess.equals(numberString)) {
+            println("You won!")
+            return
+        }
+        var correctPlaces = 0
+        var correctNumbers = 0
+        for (i in 0..3) {                           // check for non-digit characters and exact matches
+            if (!guess[i].isDigit()) {
+                println("please only use digits from 0 to 9")
+                continue@gameLoop
+            } else if (numberString[i] == guess[i]) {
+                correctPlaces++
+            }
+        }
+        for (n in 0..3) {
+            for (g in 0..3) {
+                if (guess[g] == numberString[n]){   // selected digit of guess is present in the number
+                    correctNumbers++
+                    break                           // break loop, so we don't count duplicate digits from the guess
+                }
+            }
+        }
+        println("$correctNumbers:$correctPlaces")
+    }
+}
+
+fun generateNumberString(): String {
+    var number = ""
+    outerLoop@ while (number.length < 4)
+    {
+        val nextDigit = (Random.nextInt(0,9)).digitToChar()
+        for (digit in number){
+            if(digit == nextDigit) {
+                continue@outerLoop
+            }
+        }
+        number += nextDigit
+    }
+    return number
+}
